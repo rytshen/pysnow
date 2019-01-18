764f1b0bb1a7eb7e72b7db65257f9594dfe33079
Ryan Shen <shen0416@puppetdevops.hra.nycnet>
2019-01-18 18:10:19 -0500

    Test for real

--- pysnow/incidentmacros.py
+++ pysnow/incidentmacros.py
@@ -8,19 +8,29 @@ def puppetNotReporting(instance, ci_item, behalf, date):
   if getInstance() != instance:
     setInstance(instance)
 
-  data = {
-    "description":"Puppet agent is not running",
-    "short_description": "puppet agent not running since %s" % date,
-    "subcategory": "Operating System",
-    "category": "Software",
-    "caller_id":str(getPuppetServiceUser()),
-    "cmdb_ci":ci_item,
-    "on_behalf_of":behalf,
-    "assignment_group":"a3448ed2dbcf3640948f71dabf96196d",
-    "location":"42b67d88db073e00948f71dabf961925",
-    "contact_type":"email",
-  }
-  return setDataByJson(instance, 'incident', data)
+  try:
+    query_dict = {
+      "cmdb_ci": ci_item,
+      "short_description": "puppet agent not running since %s" % date,
+    }
+    import urllib
+    qs = urllib.urlencode(query_dict)
+    existing = next(getTableResults(instance, 'incident', encodedqs=qs))
+    return existing
+  except:
+    data = {
+      "description":"Puppet agent is not running",
+      "short_description": "puppet agent not running since %s" % date,
+      "subcategory": "Operating System",
+      "category": "Software",
+      "caller_id":str(getPuppetServiceUser()),
+      "cmdb_ci":ci_item,
+      "on_behalf_of":behalf,
+      "assignment_group":"a3448ed2dbcf3640948f71dabf96196d",
+      "location":"42b67d88db073e00948f71dabf961925",
+      "contact_type":"email",
+    }
+    return setDataByJson(instance, 'incident', data)
 
 def optForPuppet(instance, ci_item, behalf):
   """Create an incident to expand opt space required for Puppet to run for ci_item"""
