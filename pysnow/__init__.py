55c74364588ad0681e45e4c1589f11dca83a2c60
Ryan Shen <shen0416@puppetdevops.hra.nycnet>
2019-01-23 12:45:05 -0500

    Prepare packaging

--- pysnow/__init__.py
+++ pysnow/__init__.py
@@ -1,4 +1,10 @@
+from pysnow.base import setInstance
+from pysnow.base import getInstance
+from pysnow.base import getDevOpsGroup
+from pysnow.base import getPuppetServiceUser
+from pysnow.cmdb import getIPComputerSysIdDict
+
+import pysnow.incidentmacros
+import pysnow.cmdb
+import pysnow.changetask
 import pysnow.base
-
-__all__ = ['base','changetask']
-
