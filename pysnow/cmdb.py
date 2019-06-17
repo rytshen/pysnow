8e0f307b1ead7eb006b7c85a9a37a1dc87f70551
Ryan Shen <shen0416@puppetdevops.hra.nycnet>
2019-06-17 12:59:28 -0400

    Improved ip address mapping that is based on last updated entries

--- pysnow/cmdb.py
+++ pysnow/cmdb.py
@@ -176,12 +176,7 @@ def getDPPoolSysIds(instance):
 def getIPComputerSysIdDict(instance):
   """returns a hashmap with primary keys of ip addresses
      to get the sys_id of the computer with that ip address"""
-  computerset = set([])
-  def setComputer(e):
-    computerset.add(e['sys_id'])
-    if ('ip_address' in e):
-      return (e['ip_address'], e['sys_id'])
-    return (None, None)
+  import datetime
 
   def isComp(e):
     try:
@@ -192,14 +187,26 @@ def getIPComputerSysIdDict(instance):
     except:
       return False
 
-  result = dict(map(setComputer, getTableResults(instance, 'cmdb_ci_computer',['sys_id','ip_address'], None, 'install_status=1')))
+  computerset = set([])
+  ipdata = {}
+  for e in getTableResults(instance, 'cmdb_ci_computer', ['sys_id','ip_address','sys_updated_on'], None, 'install_status=1')):
+    try:
+      lastupdate = datetime.datetime.strptime(e['sys_updated_on'], '%Y-%m-%d %H:%M:%S')
+      if e['ip_address'] in ipdata:
+        if ipdata[e['ip_address']][0] < lastupdate:
+          ipdata[e['ip_address']] = (lastupdate, e['sys_id'])
+      else:
+        ipdata[e['ip_address']] = (lastupdate, e['sys_id'])
+      computerset.add(e['sys_id'])
+    except:
+      pass
+  result = dict(map(lambda kv: (kv[0], kv[1][1]), ipdata.items()))
   nicfields = ['sys_id','cmdb_ci']
   niciter = getTableResults(instance, 'cmdb_ci_network_adapter', nicfields, isComp)
   nic2computer = dict(map(lambda e: (e['sys_id'], e['cmdb_ci']['value']), niciter))
-  count = 0
   for e in getTableResults(instance, 'cmdb_ci_ip_address',['nic','ip_address']):
     if ('nic' in e) and (e['nic']['value'] in nic2computer):
-      count += 1
+      if e['ip_address']
       result[e['ip_address']] = nic2computer[e['nic']['value']]
 
   return result
